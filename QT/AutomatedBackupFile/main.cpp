a111b40e49d201aefe7e4b72f4580d136c526797
Ali majidi <a.m.majidi.62@gmail.com>
2025-11-28 17:57:54 +0100

    comment css for now

--- QT/AutomatedBackupFile/main.cpp
+++ QT/AutomatedBackupFile/main.cpp
@@ -9,13 +9,13 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     
     // Load and apply stylesheet
-    QFile styleFile(":/styles.qss");
-    if (styleFile.open(QFile::ReadOnly | QFile::Text)) {
-        QTextStream stream(&styleFile);
-        QString stylesheet = stream.readAll();
-        a.setStyleSheet(stylesheet);
-        styleFile.close();
-    }
+    // QFile styleFile(":/styles.qss");
+    // if (styleFile.open(QFile::ReadOnly | QFile::Text)) {
+    //     QTextStream stream(&styleFile);
+    //     QString stylesheet = stream.readAll();
+    //     a.setStyleSheet(stylesheet);
+    //     styleFile.close();
+    // }
     
     // Set application properties for better appearance
     a.setApplicationName("Automated Backup File Manager");
